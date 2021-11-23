a803e050766867c14e8fc07e50aaca0505b752cf
Brock-99 <elmatadordelmar99@gmail.com>
2021-11-22 17:38:43 -0800

    Added Files

---
+++ src/lk/ijse/pos/dao/DataBaseAccessCode.java
@@ -0,0 +1,40 @@
+package lk.ijse.pos.dao;
+
+import lk.ijse.pos.db.DBConnection;
+import lk.ijse.pos.dto.CustomerDTO;
+import lk.ijse.pos.dto.ItemDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class DataBaseAccessCode {
+
+    //----------- Customer data-------------
+
+    public boolean saveCustomer(CustomerDTO dto) throws ClassNotFoundException, SQLException {
+        return false;
+    }
+
+    public CustomerDTO getCustomer(String id) throws ClassNotFoundException, SQLException {
+        return null;
+
+    }
+
+    public ArrayList<CustomerDTO> getAllCustomers() throws ClassNotFoundException, SQLException {
+        return null;
+    }
+
+    //----------- Item data-------------
+
+    public boolean saveItem(ItemDTO dto) throws ClassNotFoundException, SQLException {
+       return false;
+    }
+
+    public ItemDTO getItem(String id) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    public ArrayList<ItemDTO> getAllItems() throws ClassNotFoundException, SQLException {
+       return null;
+    }
+}
